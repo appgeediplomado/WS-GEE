5257040f078ca82322d41f1d6447f179a3f9347a
romansg <romans@unam.mx>
2019-01-14 19:04:26 -0600

    Algunos cambios

--- src/Gee/Config.php
+++ src/Gee/Config.php
@@ -5,7 +5,7 @@ class Config
 {
 	const DEBUG = false;
 	const DB_HOST = 'localhost';
-	const DB_USER = 'user';
+	const DB_USER = 'gee';
 	const DB_PASSWORD = 'password';
 	const DB_NAME = 'gee';
 	const BASE_URL = 'host';
