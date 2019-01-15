b854762e4e418515b4d093ec28c5226c29e71069
romansg <romans@unam.mx>
2019-01-14 18:55:20 -0600

    Update Config.php

--- src/Gee/Config.php
+++ src/Gee/Config.php
@@ -5,10 +5,10 @@ class Config
 {
 	const DEBUG = false;
 	const DB_HOST = 'localhost';
-	const DB_USER = 'gee';
-	const DB_PASSWORD = 'appgee';
+	const DB_USER = 'user';
+	const DB_PASSWORD = 'password';
 	const DB_NAME = 'gee';
-	const BASE_URL = 'http://roman.cele.unam.mx/wsgee';
+	const BASE_URL = 'host';
 	const CHARSET = 'UTF8';
 	const LONGITUD_PAGINA = 15;
 
